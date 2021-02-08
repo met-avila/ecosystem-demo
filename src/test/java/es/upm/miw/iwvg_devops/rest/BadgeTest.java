dc6e62d1707ad7a69653f699ace462682cc258a6
Meliton Tienda Avila <mtavila.1@gmail.com>
2021-02-08 15:50:51 -0600

    Adicion de espacio para badge sonar

--- src/test/java/es/upm/miw/iwvg_devops/rest/BadgeTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/BadgeTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class BadgeTest {
 
+
     @Test
     void testGenerateBadge() {
         String badge = new Badge().generateBadge("Heroku", "v2.2.0-SNAPSHOT");
